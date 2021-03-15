bfc56326b8e1c6363dfffa7cddd6819f025a0564
jnkdi <anaidik11@gmail.com>
2021-03-15 22:52:49 +0200

    add: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let templates = [];
+
+  for (item of bracketsConfig) {
+    let tmpl = item.join('');
+
+    templates.push(tmpl);
+  }
+
+  let input = str;
+
+  for (let i = 0; i < str.length / 2; i++) {
+    let length = input.length;
+  
+    for (item of templates) {
+      input = input.replace(item, '');
+    }
+
+    if (length == input.length || length == 0) break;
+  }
+
+  return input.length == 0;
 }
